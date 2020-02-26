a51db2d98cec060f1e266c03accdd248f37679ed
Shawn Urban <shawnjurban@gmail.com>
2020-02-26 01:17:46 -0500

    First pass on KOTR arena. Added objects to arena

--- SeniorProj/Assets/Scripts/ScreenWrapping.cs
+++ SeniorProj/Assets/Scripts/ScreenWrapping.cs
@@ -13,21 +13,21 @@ public class ScreenWrapping : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < -6)
+        if(transform.position.y < -5.8)
         {
-            transform.position = new Vector2(transform.position.x, -transform.position.y);
-        } else if(transform.position.y > 6)
+            transform.position = new Vector2(transform.position.x, 5.7f);
+        } else if(transform.position.y > 5.8)
         {
-            transform.position = new Vector2(transform.position.x, -transform.position.y);
+            transform.position = new Vector2(transform.position.x, -5.7f);
         }
 
         if(transform.position.x < -9.5)
         {
-            transform.position = new Vector2(-transform.position.x, transform.position.y);
+            transform.position = new Vector2(9.45f, transform.position.y);
         }
         else if(transform.position.x > 9.5)
         {
-            transform.position = new Vector2(-transform.position.x, transform.position.y);
+            transform.position = new Vector2(-9.45f, transform.position.y);
         }
     }
 }
