288e44bbdf155447f6b4b240ed3e1d627a29b816
Léo Melendez <leo.melendez198@gmail.com>
2020-05-01 18:18:06 -0400

    Finished item spawning location functionality

--- SeniorProj/Assets/Scripts/Object Scripts/ObjectSpawner.cs
+++ SeniorProj/Assets/Scripts/Object Scripts/ObjectSpawner.cs
@@ -8,6 +8,7 @@ public class ObjectSpawner : MonoBehaviour
     public List<GameObject> bombSpawner = new List<GameObject>();
 
     public List<Vector3> spawnPos = new List<Vector3>();
+    public List<GameObject> spawnPoints = new List<GameObject>();
 
     public GameObject bombPrefab;
 
@@ -33,12 +34,30 @@ public class ObjectSpawner : MonoBehaviour
                 bombSpawner[i].GetComponent<Bomb>().hasSpawned = true;
                 bombSpawner.Insert(bombSpawner.IndexOf(bombSpawner[i]), newBomb);
                 bombSpawner.RemoveAt(bombSpawner.IndexOf(bombSpawner[i]) + 1);
-                
-                newBomb.transform.position = new Vector3(spawnPos[i].x, spawnPos[i].y, spawnPos[i].z);
+                GetNewSpawnPoint(newBomb);
+                //newBomb.transform.position = new Vector3(spawnPos[i].x, spawnPos[i].y, spawnPos[i].z);
                 //spawnPos[Random.Range(0, spawnPos.Count)];
                 //new Vector3(spawnPos[i].x, spawnPos[i].y, spawnPos[i].z);
                 
             }
         }
+    }
+
+    void GetNewSpawnPoint(GameObject newBomb) {
+
+
+        for (int i = Random.Range(0,spawnPoints.Count - 1); i < spawnPoints.Count;) {
+            if (spawnPoints[i].GetComponent<spawnPoint>().isEmpty) {
+                 newBomb.transform.position = spawnPoints[i].transform.position;
+                Debug.Log("Empty spawn point found.");
+                return;
+            }
+            else{
+                    Debug.Log("Respawn point was not null, checking for a new point...");
+
+                i++;
+            }
+        }
+
     }
 }
---
+++ SeniorProj/Assets/Scripts/spawnPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour
+{
+    CircleCollider2D _collider;
+    public bool isEmpty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isEmpty = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected");
+
+        if(collision.tag == "Bomb" || collision.tag == "Knife"){
+            isEmpty = false;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Bomb" || collision.tag == "Knife")
+        {
+            isEmpty = true;
+        }
+    }
+}
